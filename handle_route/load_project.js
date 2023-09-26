cc8911f5695312c8f4eeea6928ba0210e0df64c6
xinyongheng <xinheng8182@163.com>
2023-09-26 14:13:32 +0800

    changeSaveProject

--- handle_route/load_project.js
+++ handle_route/load_project.js
@@ -17,12 +17,15 @@ function fillProject(projectId) {
 left join unit on winning_unit.unit_id=unit.id and winning_unit.project_id=${projectId}`,
         { type: QueryTypes.SELECT, mapToModel: true, model: WinningUnit });
     // 投标单位
-    var buildUnits = sequelize.query(`select build_unit.*,unit.name as unitName from build_unit left join unit on build_unit.unit_id=unit.id and build_unit.project_id=${projectId}`,
+    var buildUnits = sequelize.query(
+        `select build_unit.*,unit.name as unitName from build_unit 
+left join unit on build_unit.unit_id=unit.id and build_unit.project_id=${projectId}`,
         { type: QueryTypes.SELECT, mapToModel: true, model: BuildUnit });
     // 分包单位
-    var subcontractingUnits = sequelize.query(`select subcontracting_unit.*,unit.name as unitName 
-left join unit on subcontracting_unit.unit_id=unit.id 
-left join subcontracting_project_mapping on subcontracting_project_mapping.subcontracting_unit_id=subcontracting_unit.id and subcontracting_project_mapping.project_id=${projectId}`,
+    var subcontractingUnits = sequelize.query(
+        `select subcontracting_unit.*,unit.name as unitName 
+left join subcontracting_project_mapping on subcontracting_project_mapping.subcontracting_unit_id=subcontracting_unit.id and subcontracting_project_mapping.project_id=${projectId} 
+left join unit on subcontracting_unit.unit_id=unit.id`,
         { type: QueryTypes.SELECT, mapToModel: true, model: BuildUnit });
     return { winningUnits, buildUnits, subcontractingUnits }
 }
